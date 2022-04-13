05d48a0d441ffbab8ccd8ef3f086949eca901878
Felipe <machadofelipe2016@outlook.com>
2022-04-13 17:57:54 -0300

    c10- adding and create usestate and data, setdatw

--- context/src/App.js
+++ context/src/App.js
@@ -1,13 +1,19 @@
+import React, {useState} from 'react'
 import  firebase from './firebase'
 import './App.css'
 
 function App() {
+  const [data, setdata] = useState({})
+  
   const ref = firebase.database().ref('test')
- ref.on('value', snapshoot => { console.log(snapshoot.val())} )
+  ref.on('value', snapshoot => {
+     console.log(snapshoot.val())
+     setdata(snapshoot.val())
+    })
   
   return (
     <div >
-    
+       <pre> {JSON.stringify(data)} </pre>
     </div>
   )
 }
