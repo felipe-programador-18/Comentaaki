fd05e5ccd83b31b70f0c14ceb86301f1e04e5ccb
Felipe <machadofelipe2016@outlook.com>
2022-04-13 19:12:42 -0300

    c16- create inject dependence to create hook personalities

--- context/src/App.js
+++ context/src/App.js
@@ -2,6 +2,19 @@ import React, {useState, useEffect} from 'react'
 import  firebase from './firebase'
 import './App.css'
 
+const Usedatabase = endpoints => {
+  const [data, setdata] = useState({})
+  useEffect(() => {
+    const ref = firebase.database().ref('test')
+  ref.on('value', snapshoot => {
+     //console.log(snapshoot.val())
+     setdata(snapshoot.val())
+    })
+    return () => {
+     ref.off()
+    }
+  }, [])
+}
 
 const Comments = () => {
   const [data, setdata] = useState({})
