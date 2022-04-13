f1b189082bdfe0bbff07091e778c1294e1921942
Felipe <machadofelipe2016@outlook.com>
2022-04-13 16:49:38 -0300

    c2- create file firebase to adding authentication

---
+++ context/src/firebase.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import firebase from 'firebase/app'
+import 'firebase/database' 
+const firebaseConfig = {
+    apiKey: "AIzaSyClS5AzqPl31lT22vM4HEwnVRvZAfyMSx0",
+    authDomain: "comentaaki-appi.firebaseapp.com",
+    projectId: "comentaaki-appi",
+    storageBucket: "comentaaki-appi.appspot.com",
+    messagingSenderId: "781761946955",
+    appId: "1:781761946955:web:0d24a860885b573548b4ce"
+  };
+
+firebase.initializeApp(firebaseConfig)
+
+
+
