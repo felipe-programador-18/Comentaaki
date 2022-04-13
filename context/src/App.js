6b152bf4ca247cc7b6b87ad3988e53fff6b24ad1
Felipe <machadofelipe2016@outlook.com>
2022-04-13 17:54:14 -0300

    c8- change function before have been outside state now is inside state

--- context/src/App.js
+++ context/src/App.js
@@ -1,12 +1,10 @@
 import  firebase from './firebase'
 import './App.css'
 
-//create structure about testing connection about firebase
-const ref = firebase.database().ref('test')
-ref.on('value', snapshoot => { console.log(snapshoot.val())} )
-
-
 function App() {
+  const ref = firebase.database().ref('test')
+ ref.on('value', snapshoot => { console.log(snapshoot.val())} )
+  
   return (
     <div >
     
