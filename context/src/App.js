454b7c8df454fa5b909472d688a8df9464941878
Felipe <machadofelipe2016@outlook.com>
2022-04-14 17:45:32 -0300

    c24- adding informations and if anf else

--- context/src/App.js
+++ context/src/App.js
@@ -21,6 +21,16 @@ const Usedatabase = endpoints => {
 //create function to inject another dates
 const UsedatabasePush = endpoints => {
    const [status, setstatus] = useState('')
+    const save = data => {
+      const ref = firebase.database().ref(endpoints)
+      ref.push(data,  err => {
+        if(err){
+          setstatus('error aplication')
+        }else{
+          setstatus('SUCCESS')
+        }
+      })
+    }
 }
 
 
