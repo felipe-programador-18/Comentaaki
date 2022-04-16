99a0586e6db7de63d8168b02e8974fab3f6df8c2
Felipe <machadofelipe2016@outlook.com>
2022-04-16 17:41:23 -0300

    c53-thought in shapa authentication

--- context/src/App.js
+++ context/src/App.js
@@ -13,7 +13,12 @@ import './App.css'
    })
 })
 */
-firebase.auth()
+firebase.auth().onAuthStateChanged( users => {
+  if(users){
+    console.log(users.displayName)
+    users.updateProfile({displayName:'Felipe Martins Programer 18'})
+  }
+})
 
 
 // this is tipy inject dependence...
