32263b60e1e16ab32a294d85cd514c205a97770d
Felipe <machadofelipe2016@outlook.com>
2022-04-16 15:00:11 -0300

    c38- adding object react to adding name users

--- context/src/App.js
+++ context/src/App.js
@@ -37,7 +37,7 @@ const UsedatabasePush = endpoints => {
 const Comment = ({comment}) =>{
   return (
     <div>
-     {comment.content}
+     {comment.content} por : {comment.users.name}
     </div>
   )
 }
