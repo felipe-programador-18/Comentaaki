9bed6c3d73515dace9193a7259f8343f78593864
Felipe <machadofelipe2016@outlook.com>
2022-04-18 17:51:43 -0300

    c81-rearrange file and set up new modify

--- context/src/Comment.js
+++ context/src/Comment.js
@@ -8,9 +8,11 @@ import { AuthContext } from './auth'
 // create new comments to adding another way
 const Comment = ({comment}) =>{
   // remember i am caught value count flag of state count of setcount!!!
-  
+   
+  const auth = useContext(AuthContext)
   return (
       <div>
+        {JSON.stringify(auth)}
        {comment.content} por : {comment.users.name} em : <Time TIMESTAMP={comment.createAT}/>
       </div>
     )
