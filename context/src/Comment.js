ef97a3cccdd6d9b437b802abddec2611ea4521c8
Felipe <machadofelipe2016@outlook.com>
2022-04-18 17:52:00 -0300

    c82- testing file auth

--- context/src/Comment.js
+++ context/src/Comment.js
@@ -2,9 +2,6 @@ import React, {useContext} from 'react'
 import Time from './Time'
 import { AuthContext } from './auth'
 
-
-
-
 // create new comments to adding another way
 const Comment = ({comment}) =>{
   // remember i am caught value count flag of state count of setcount!!!
--- context/src/auth.js
+++ context/src/auth.js
@@ -1,3 +1,13 @@
 import React, {useState} from 'react'
  
 export const AuthContext = React.createContext()
+//theorical this gonna control all state aplications!!!
+export const AuthProvider = ({children}) => {
+    return (
+        <AuthContext.Provider value={ {isAuth:1}} >
+         {children}
+        </AuthContext.Provider>
+    )
+}
+
+
