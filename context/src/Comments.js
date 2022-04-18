a9546ce25481f6a331ae17870014f517631d488f
Felipe <machadofelipe2016@outlook.com>
2022-04-18 16:02:24 -0300

    c66- create file outside called Commets

---
+++ context/src/Comments.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Usedatabase } from "./database"
+
+const Comments = () => {
+    const data = Usedatabase('comments')
+    // this !data meanings if value is null comparitive
+    if(!data){
+     return <p>Anymore comments send moment</p>
+    }
+    //create variable to caught all ids
+     const ids = Object.keys(data)
+     if(ids.length === 0){
+       return <p>Loading .....</p>
+     }
+    return ids.map(id => {
+       return <Comment key={id} comment={data[id]} />
+    })
+  }
+  
+
+export default Comments  
