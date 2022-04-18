b456ec571f3c66a8ea8d205aeb287714b9bdade5
Felipe <machadofelipe2016@outlook.com>
2022-04-18 18:03:15 -0300

    create usestate and useeffect to set dates

--- context/src/auth.js
+++ context/src/auth.js
@@ -1,6 +1,23 @@
-import React, {useState} from 'react'
- 
+import React, {useState, useEffect} from 'react'
+import firebase from './firebase' 
+
 export const AuthContext = React.createContext()
+
+
+const UseisAuth = () => {
+ const [user, setusers] = useState({}) 
+  useEffect(() => {
+  firebase.auth().onAuthStateChanged( Currentusers => {
+  if(Currentusers){
+    
+  }else{
+
+   }
+  })
+ }, [])
+}
+
+
 //theorical this gonna control all state aplications!!!
 export const AuthProvider = ({children}) => {
     return (
