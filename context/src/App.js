c71b50e06f58c7c0c8ffcdd396b9a9b318c6a752
Felipe <machadofelipe2016@outlook.com>
2022-04-18 16:15:19 -0300

    c71- more littie bit rearrange app

--- context/src/App.js
+++ context/src/App.js
@@ -25,19 +25,6 @@ firebase.auth().onAuthStateChanged( users => {
 */
 
 
-
-const Time = ({TIMESTAMP}) => {
-   const date = new Date(TIMESTAMP)
-   const hours = date.getHours()
-   const minute = '0'+ date.getMinutes()
-   const second = '0' + date.getSeconds()
-   const day = '0' + (date.getDate())
-   const month = '0' + (date.getMonth()+1)
-   const year =  (date.getFullYear())
-  return `${day.substr(-2)}/${month.substr(-2)}/${year} ${hours}:${minute.substr(-2)}:${second.substr(-2)}`
-}
-
-
 function App() {
 
   return (
