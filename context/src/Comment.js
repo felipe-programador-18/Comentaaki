e286d79261e3b33dd1b3d321ab76cfa2fd7a8821
Felipe <machadofelipe2016@outlook.com>
2022-04-18 17:36:40 -0300

    c79- using little bit usecontext

--- context/src/Comment.js
+++ context/src/Comment.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import Time from './Time'
+import { AuthContext } from './auth'
+
+
+
 
 // create new comments to adding another way
 const Comment = ({comment}) =>{
-    return (
+  // remember i am caught value count flag of state count of setcount!!!
+  
+  return (
       <div>
        {comment.content} por : {comment.users.name} em : <Time TIMESTAMP={comment.createAT}/>
       </div>
