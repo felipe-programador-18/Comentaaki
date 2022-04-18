28382b83666e58e9f1226ac858bc191a10004868
Felipe <machadofelipe2016@outlook.com>
2022-04-18 17:36:09 -0300

    c78-making testing about authentication

--- context/src/App.js
+++ context/src/App.js
@@ -32,7 +32,6 @@ function App() {
     <div > 
       <NewComments/>     
       <Comments /> 
-       <button onClick={()=> setcout(count+1)}></button>
     </div>
     </AuthContext.Provider>
   )
