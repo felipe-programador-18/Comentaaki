b9d2287a89b0743dd1f795ff77f548344142e8e5
Felipe <machadofelipe2016@outlook.com>
2022-04-18 18:43:20 -0300

    c84-create new modify

--- context/src/App.js
+++ context/src/App.js
@@ -1,20 +1,8 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 import './App.css'
 import { AuthProvider } from './auth'
 import NewComments from './NewComments'
 import Comments from './Comments'
-
-// create ways of access with autentication
-
-/*firebase
-.auth()
-.createUserWithEmailAndPassword('machadofelipe2016@outlook.com','123martins')
-.then(users => {
-   return users.updateProfile({
-     displayName: 'Felipe Programmer'
-   })
-})
-*/
 
 function App() {
  
