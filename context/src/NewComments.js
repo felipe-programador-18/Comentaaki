5d9f961567046a126cc807e839481e9c4ceb2c3c
Felipe <machadofelipe2016@outlook.com>
2022-04-18 15:39:41 -0300

    c56- export file and import react and dependence

---
+++ context/src/NewComments.js
@@ -0,0 +1,30 @@
+import REACT from 'react'
+
+const NewComments = props => {
+    const [, save] = UsedatabasePush('comments')
+    const [comment, setcomment] = useState('')
+    const CreateComments = () => {
+      if( comment!== ''){
+      save({
+        content: comment,
+        createAT: firebase.database.ServerValue.TIMESTAMP,
+        users:{
+          id:"1",
+          name:'Felipe programmer 18'
+        }
+      })
+      // theoriotical this serve to controll state of text area
+      setcomment('')
+      }
+    }
+  
+    return (
+      <div>
+        <textarea value ={comment} onChange={evt => setcomment(evt.target.value)}   />
+      <button onClick={CreateComments} > Toggle</button>
+    </div>
+    )
+  }
+  
+
+export default NewComments 
