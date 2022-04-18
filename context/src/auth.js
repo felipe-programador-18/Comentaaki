2f5b1d4f3a02b7226072e6b4cbee19b72d598d53
Felipe <machadofelipe2016@outlook.com>
2022-04-18 18:43:45 -0300

    c85- solve issues

--- context/src/auth.js
+++ context/src/auth.js
@@ -3,25 +3,60 @@ import firebase from './firebase'
 
 export const AuthContext = React.createContext()
 
-
-const UseisAuth = () => {
- const [user, setusers] = useState({}) 
+const UseGetUser = () => {
+ const [users, setusers] = useState(null) 
   useEffect(() => {
-  firebase.auth().onAuthStateChanged( Currentusers => {
-  if(Currentusers){
-    
+  firebase.auth().onAuthStateChanged( currentusers => {
+  if(currentusers){
+     setusers(currentusers)
   }else{
-
+    setusers(null)
    }
   })
  }, [])
+ return users
+}
+
+// create ways of access with autentication
+const UserCreateUser = () => {
+ const [state, setstate] = useState({
+     error:'',
+     success:''
+ })
+ const CreateUser = (email, password) => {
+ firebase
+  .auth()
+  .createUserWithEmailAndPassword(email, password)
+  .then(users => {
+    if(users){
+        setstate({
+            ...state,
+            success : 'ok',
+
+        })
+    }
+   })
+ }
+  () .catch(err => {
+     setstate({
+         ...state,
+         error: err.message
+     })
+   })
+   return [state, CreateUser]
 }
 
 
 //theorical this gonna control all state aplications!!!
 export const AuthProvider = ({children}) => {
-    return (
-        <AuthContext.Provider value={ {isAuth:1}} >
+  const users = UseGetUser()
+  const [CreateUserstate, CreateUser] = UserCreateUser()
+  return(
+        <AuthContext.Provider value={ {users,
+        CreateUser:{
+            CreateUserstate, CreateUser
+        }
+        }} >
          {children}
         </AuthContext.Provider>
     )
