0840c02a09300aae4b1530bbc58ca165b6810ecf
Felipe <machadofelipe2016@outlook.com>
2022-04-19 18:10:49 -0300

    c104- finnaly project

--- context/src/UserInfo.js
+++ context/src/UserInfo.js
@@ -1,23 +1,32 @@
 import React, { useState ,useContext} from 'react'
 import { AuthContext } from './auth'
 
+//create one function to easily all process!!! 
+//practice so much about that
+const Formdisplay = ({ displayName}) => {
+    const [newDisplayName, setdisplayname] = useState(displayName)
+    return ( <>
+        <input type='text' value={newDisplayName}  />
+        <button>Save Display Name</button>
+    </>
+ )
+}
+
+
 const UserInfo = () => {
- const [newDisplayName, setdisplayname] = useState('')
+ const auth = useContext(AuthContext)
  // here i am check if don't having nothing, not go show none!
  if(auth.users  === null){
     return null
   }  
 
- const {displayName} = auth.users
- const [alternativeDisplayName] = auth.users.email.split('@')
- const dn = displayName || alternativeDisplayName
-
- const auth = useContext(AuthContext)
-
+  const {displayName} = auth.users
+  const [alternativeDisplayName] = auth.users.email.split('@')
+  const dn = displayName || alternativeDisplayName
+  
   return( <>
       <p> Hi there!! {dn} !  </p>
-      <input type='text' value={newDisplayName}  />
-      <button onClick={}>Save Display Name</button>
+      <Formdisplay displayName={dn} />
   </>
   )
 }
