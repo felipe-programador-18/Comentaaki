d3d563b60004394020afde8e971c9d39c194cbc9
Felipe <machadofelipe2016@outlook.com>
2022-04-19 18:01:16 -0300

    c103- rearrange all code

--- context/src/UserInfo.js
+++ context/src/UserInfo.js
@@ -2,14 +2,22 @@ import React, { useState ,useContext} from 'react'
 import { AuthContext } from './auth'
 
 const UserInfo = () => {
- const auth = useContext(AuthContext)
+ const [newDisplayName, setdisplayname] = useState('')
+ // here i am check if don't having nothing, not go show none!
  if(auth.users  === null){
     return null
-  }
-  const {displayName} = auth.users
-  const [alternativeDisplayName] = auth.users.email.split('@')
+  }  
+
+ const {displayName} = auth.users
+ const [alternativeDisplayName] = auth.users.email.split('@')
+ const dn = displayName || alternativeDisplayName
+
+ const auth = useContext(AuthContext)
+
   return( <>
-      <p> Hi there!! {displayName || alternativeDisplayName} !  </p>
+      <p> Hi there!! {dn} !  </p>
+      <input type='text' value={newDisplayName}  />
+      <button onClick={}>Save Display Name</button>
   </>
   )
 }
