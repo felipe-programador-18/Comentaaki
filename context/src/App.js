70bcbef15a2576766c8f368451007986cfed5e25
Felipe <machadofelipe2016@outlook.com>
2022-04-19 17:16:05 -0300

    c95- check up and started create auth to see users is authentication

--- context/src/App.js
+++ context/src/App.js
@@ -1,12 +1,13 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import './App.css'
-import { AuthProvider } from './auth'
+import { AuthContext, AuthProvider } from './auth'
 import NewComments from './NewComments'
 import Comments from './Comments'
 import CreateUser from './CreateUser'
+import { useContext } from 'react'
 
 function App() {
- 
+  const auth = useContext( AuthContext)
   return (
     <AuthProvider>
     <div > 
