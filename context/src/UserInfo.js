d0ef5bfe9fa2f81e3c793d3dd137a44798fd0888
Felipe <machadofelipe2016@outlook.com>
2022-04-19 17:48:35 -0300

    100-create useringo to caught auth and testing users logs

---
+++ context/src/UserInfo.js
@@ -0,0 +1,17 @@
+import React, {useContext} from 'react'
+import { AuthContext } from './auth'
+
+const UserInfo = () => {
+ const auth = useContext(AuthContext)
+ if(auth.users  === null){
+    return null
+  }
+  const {displayName} = auth.users
+  const [alternativeDisplayName] = auth.users.email.split('@')
+  return(
+      <p> Hi there!! {displayName || alternativeDisplayName} !  </p>
+  )
+}
+
+
+export default UserInfo
