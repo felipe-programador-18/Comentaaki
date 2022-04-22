d943eabbc31aabc96a2bffbd5ab2a7168611cf07
Felipe <machadofelipe2016@outlook.com>
2022-04-22 15:46:08 -0300

    112- create input to adding new users with email and name and aadding h3 to give new rearrange all code

--- context/src/CreateUser.js
+++ context/src/CreateUser.js
@@ -1,10 +1,13 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
 import { AuthContext } from './auth'
 
 const CreateUser = () => {
   const auth = useContext(AuthContext)
     return ( <>
+    <h3>Create New account</h3>
     {JSON.stringify(auth.CreateUser)}
+    <input type='text' placeholder='Your email:'/>
+    <input type='text' placeholder='Your Password:'/>
      <button onClick={() => { auth.CreateUser.CreateUser('machadoprogrammerfelipe2016@outlook.com', 'martins18')}} >+++</button>
    
    </>)
