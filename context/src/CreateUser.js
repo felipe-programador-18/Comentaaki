e13ee08be4ca01a32099fb6618d1959b7dc3f10a
Felipe <machadofelipe2016@outlook.com>
2022-04-22 15:48:55 -0300

    c113-create one destrucment assyment to receive two values

--- context/src/CreateUser.js
+++ context/src/CreateUser.js
@@ -3,7 +3,10 @@ import { AuthContext } from './auth'
 
 const CreateUser = () => {
   const auth = useContext(AuthContext)
-    return ( <>
+  //here down i create usestate to caught all date of both email and name
+  const [form, setform] = useState({email:'', passwd:''})
+
+  return ( <>
     <h3>Create New account</h3>
     {JSON.stringify(auth.CreateUser)}
     <input type='text' placeholder='Your email:'/>
