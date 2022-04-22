4156978c590875b895e853928d422a558185f841
Felipe <machadofelipe2016@outlook.com>
2022-04-22 16:16:41 -0300

    c117- finnaly gotten solve issues

--- context/src/CreateUser.js
+++ context/src/CreateUser.js
@@ -3,7 +3,6 @@ import { AuthContext } from './auth'
 
 const CreateUser = () => {
   const auth = useContext(AuthContext)
-  
   //here down i create usestate to caught all date of both email and name
   const [form, setform] = useState({email:'', passwd:''})
   //passes two parameter to receive field and events
@@ -14,16 +13,16 @@ const CreateUser = () => {
      [campo]: evt.target.value
     })
   } 
-
   return ( <>
     <h3>Create New account</h3>
     {
-      (auth.CreateUser.CreateUserState.error) !== '' && 
-      <p> {auth.CreateUser.CreateUserState.error} </p>
+      auth.CreateUser.CreateUser.error !== '' && 
+      <p> {auth.CreateUser.CreateUser.error} </p>
     
     }
     <input type='text' placeholder='Your email:' value={form.email} onChange={onChange('email')}  />
-    <input type='text' placeholder='Your Password:' value={form.passwd} onChange={onChange('password')}  />
+    <input type='password' placeholder='Your Password:' value={form.passwd} onChange={onChange('passwd')}  />
+     
      <button onClick={() => { auth.CreateUser.CreateUser(form.email, form.passwd)}} >Create accounte !!</button>
    
    </>)
