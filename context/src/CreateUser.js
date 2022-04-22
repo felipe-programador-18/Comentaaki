b21a88d439e49109f66b298ab582e4faf1b81ebc
Felipe <machadofelipe2016@outlook.com>
2022-04-22 15:52:08 -0300

    c114- create function ta receive to paramether field and evt

--- context/src/CreateUser.js
+++ context/src/CreateUser.js
@@ -3,8 +3,17 @@ import { AuthContext } from './auth'
 
 const CreateUser = () => {
   const auth = useContext(AuthContext)
+  
   //here down i create usestate to caught all date of both email and name
   const [form, setform] = useState({email:'', passwd:''})
+  //passes two parameter to receive field and events
+  const onChange = campo => evt => {
+    //theoritecal i need use spring operator to caught inside out all form
+    setform({
+     ...form,
+     [campo]: evt.target.value
+    })
+  } 
 
   return ( <>
     <h3>Create New account</h3>
